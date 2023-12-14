9802ed15969fabfc6c2cbc11173766a0e480297b
Hager <hajer.ibr@gmail.com>
2023-12-14 08:49:09 +0200

    load base url from ConfigUtils

--- src/test/java/com/qacart/todo/pages/LoginPage.java
+++ src/test/java/com/qacart/todo/pages/LoginPage.java
@@ -1,9 +1,15 @@
 package com.qacart.todo.pages;
 
 import com.qacart.todo.base.BasePage;
+import com.qacart.todo.utils.ConfigUtils;
+import com.qacart.todo.utils.PropertiesUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.Properties;
+
+import static com.qacart.todo.constants.ConfigConstants.BASE_URL;
 
 public class LoginPage extends BasePage {
 
@@ -21,7 +27,7 @@ public class LoginPage extends BasePage {
     }
 
     public LoginPage load(){
-         driver.get("http://qacart-todo.herokuapp.com/");
+         driver.get(ConfigUtils.getBaseUrl());
          return this;
     }
     public ToDoPage login(String email, String password){

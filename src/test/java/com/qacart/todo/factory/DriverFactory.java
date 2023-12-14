8282fab003e0c72942fb895b2211455c59a8d76b
Hager <hajer.ibr@gmail.com>
2023-12-14 08:22:05 +0200

    Add DriverFactory

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -3,16 +3,38 @@ package com.qacart.todo.factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
+
+import static com.qacart.todo.constants.ConfigConstants.*;
 
 public class DriverFactory {
 
     public WebDriver initializeDriver(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        String browser = System.getProperty(BROWSER, CHROME);
+        WebDriver driver;
+        switch (browser) {
+            case CHROME -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case FIREFOX -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            case EDGE -> {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+            }
+            default -> throw new RuntimeException("The browser is not supported!");
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         return driver;
     }
+
+
 }

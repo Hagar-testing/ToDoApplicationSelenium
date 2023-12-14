5829db43e8b8ff0912afe6fa46abcb7f2e138e2e
Hager <hajer.ibr@gmail.com>
2023-12-14 08:21:04 +0200

    Add ConfigConstants file

---
+++ src/test/java/com/qacart/todo/constants/ConfigConstants.java
@@ -0,0 +1,11 @@
+package com.qacart.todo.constants;
+
+public class ConfigConstants {
+
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String EDGE = "edge";
+    public static final String BROWSER = "browser";
+    public static final String BASE_URL = "baseUrl";
+    public static final String HEADLESS = "headless";
+}

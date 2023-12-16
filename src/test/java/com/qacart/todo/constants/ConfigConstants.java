c7d956fe7031953b010eb069bb158707e040ed86
Hager <hajer.ibr@gmail.com>
2023-12-16 07:41:54 +0200

    handle getting environment from system

--- src/test/java/com/qacart/todo/constants/ConfigConstants.java
+++ src/test/java/com/qacart/todo/constants/ConfigConstants.java
@@ -10,4 +10,12 @@ public class ConfigConstants {
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
     public static final String HEADLESS = "headless";
+    public static final String ENV = "env";
+    public static final String PRODUCTION = "production";
+    public static final String STAGING = "staging";
+    public static final String CONFIG_PATH = "src/test/java/com/qacart/todo/config/";
+    public static final String PRODUCTION_PROPERTIES_PATH = CONFIG_PATH + "production.properties";
+    public static final String STAGING_PROPERTIES_PATH =  CONFIG_PATH + "stg.properties";
+
+
 }
--- src/test/java/com/qacart/todo/utils/ConfigUtils.java
+++ src/test/java/com/qacart/todo/utils/ConfigUtils.java
@@ -2,16 +2,28 @@ package com.qacart.todo.utils;
 
 import java.util.Properties;
 
-import static com.qacart.todo.constants.ConfigConstants.BASE_URL;
+import static com.qacart.todo.constants.ConfigConstants.*;
 
 public class ConfigUtils {
 
     private static final Properties properties;
 
     static {
-        properties = PropertiesUtils.loadProperties("src/test/java/com/qacart/todo/config/stg.properties");
+        properties = PropertiesUtils.loadProperties(getPropertiesFilePath());
     }
 
+    private static String getPropertiesFilePath()  {
+        return switch (getEnv()) {
+            case PRODUCTION -> PRODUCTION_PROPERTIES_PATH;
+            case STAGING -> STAGING_PROPERTIES_PATH;
+            default -> throw new RuntimeException("Environment is not supported");
+        };
+    }
+
+    public static String getEnv(){
+        String envFromSys = System.getProperty(ENV);
+        return envFromSys != null ? envFromSys : STAGING;
+    }
 
     public static String getBaseUrl(){
         String urlFromSys = System.getProperty(BASE_URL);

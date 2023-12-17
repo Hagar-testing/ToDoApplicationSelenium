c35b8e5f6e70f35b3f293df49b0cdfdfc91bf520
Hager <hagar.testing@gmail.com>
2023-12-17 08:56:16 +0200

    Add injectCookiesToBrowser function to baseTest

--- src/test/java/com/qacart/todo/base/BaseTest.java
+++ src/test/java/com/qacart/todo/base/BaseTest.java
@@ -1,8 +1,12 @@
 package com.qacart.todo.base;
 
 import com.qacart.todo.factory.DriverFactory;
+import com.qacart.todo.utils.CookieUtils;
+import io.restassured.http.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.List;
 
 public class BaseTest {
 
@@ -17,5 +21,13 @@ public class BaseTest {
 
     public void tearDown(){
         driver.quit();
+    }
+
+    public void injectCookiesToBrowser(List<Cookie> restAssuredCookies){
+
+        List<org.openqa.selenium.Cookie> seleniumCookies = CookieUtils.convertRestAssuredCookiesToSeleniumCookies(restAssuredCookies);
+        for (org.openqa.selenium.Cookie seleniumCookie : seleniumCookies){
+            driver.manage().addCookie(seleniumCookie);
+        }
     }
 }

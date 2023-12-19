cf326f08efbb8661267d299fef06c2351528e614
Hager <hagar.testing@gmail.com>
2023-12-19 09:39:46 +0200

    Add Task class

---
+++ src/test/java/com/qacart/todo/objects/Task.java
@@ -0,0 +1,28 @@
+package com.qacart.todo.objects;
+
+public class Task {
+
+    private String item;
+    private Boolean isCompleted;
+
+    public Task(String item, Boolean isCompleted) {
+        this.item = item;
+        this.isCompleted = isCompleted;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public Boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(Boolean completed) {
+        isCompleted = completed;
+    }
+}

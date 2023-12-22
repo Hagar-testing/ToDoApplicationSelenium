f7653df4f4fb0a0531421c1a258e929a2a2f8848
Hager <hagar.testing@gmail.com>
2023-12-22 10:05:25 +0200

    Handle adding screenshot to allure report

--- src/test/java/com/qacart/todo/base/BaseTest.java
+++ src/test/java/com/qacart/todo/base/BaseTest.java
@@ -2,11 +2,20 @@ package com.qacart.todo.base;
 
 import com.qacart.todo.factory.DriverFactory;
 import com.qacart.todo.utils.CookieUtils;
+import io.qameta.allure.Allure;
 import io.restassured.http.Cookie;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class BaseTest {
@@ -29,7 +38,10 @@ public class BaseTest {
 
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result){
+        String testCaseName = result.getMethod().getMethodName();
+        File destFile = new File("target" + File.separator + "screenshots" + File.separator + testCaseName + ".png");
+        takeScreenshot(destFile);
         getDriver().quit();
     }
 
@@ -38,6 +50,17 @@ public class BaseTest {
         List<org.openqa.selenium.Cookie> seleniumCookies = CookieUtils.convertRestAssuredCookiesToSeleniumCookies(restAssuredCookies);
         for (org.openqa.selenium.Cookie seleniumCookie : seleniumCookies){
             getDriver().manage().addCookie(seleniumCookie);
+        }
+    }
+
+    public void takeScreenshot(File destFile){
+        File file = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, destFile);
+            InputStream inputStream = new FileInputStream(destFile);
+            Allure.addAttachment("screenshot",inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
